072dfe9bf2fd59824bf73e270e0ef5c33f7d63dc
Takaeroid <takenokoroid@gmail.com>
2020-03-23 22:14:40 +0900

    arrowsの挙動完成

--- src/components/TransitionArrow.vue
+++ src/components/TransitionArrow.vue
@@ -1,16 +1,50 @@
 <template>
   <div>
-    <v-btn class="mr-5" color="success" text icon height="80" width="80">
+    <v-btn class="mr-5" color="success" text icon height="80" width="80" @click="backPage">
       <v-icon size="80">mdi-arrow-left-thick</v-icon>
     </v-btn>
-    <v-btn class="ml-5" color="success" text icon height="80" width="80">
+    <v-btn class="ml-5" color="success" text icon height="80" width="80" @click="nextPage">
       <v-icon size="80">mdi-arrow-right-thick</v-icon>
     </v-btn>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      pages: this.$store.getters.getPages
+    };
+  },
+  methods: {
+    /**
+     * storeにあるindexをインクリメント
+     * indexをget
+     * pages[index]で場所を指定して注入
+     * もっとスマートに書き直すかも...
+     */
+    nextPage: function() {
+      this.$store.commit("increment");
+      let index = this.$store.getters.getIndex;
+      if (index < this.pages.length) {
+        console.log(index);
+        this.$router.push(this.pages[index]);
+      } else {
+        this.$store.commit("decrement");
+      }
+    },
+    backPage: function() {
+      this.$store.commit("decrement");
+      let index = this.$store.getters.getIndex;
+      if (index > -1) {
+        console.log(index);
+        this.$router.push(this.pages[index]);
+      } else {
+        this.$store.commit("increment");
+      }
+    }
+  }
+};
 </script>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/:page",
+    path: "/",
     name: "Home",
     component: Home
   },
